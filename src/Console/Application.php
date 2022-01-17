b0c2ca896dd52cfdbe63348647966903c5c658c1
Nicolas PHILIPPE <nikophil@gmail.com>
2022-01-17 14:56:24 +0100

    fix: add missing return type for sf6 compatibility

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -22,6 +22,7 @@ use CacheTool\Monolog\ConsoleHandler;
 use Monolog\Logger;
 use Symfony\Component\Console\Application as BaseApplication;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -58,7 +59,7 @@ class Application extends BaseApplication
     /**
      * {@inheritdoc}
      */
-    protected function getDefaultCommands()
+    protected function getDefaultCommands(): array
     {
         $commands = parent::getDefaultCommands();
         $commands[] = new CacheToolCommand\SelfUpdateCommand(
@@ -101,7 +102,7 @@ class Application extends BaseApplication
     /**
      * {@inheritDoc}
      */
-    protected function getDefaultInputDefinition()
+    protected function getDefaultInputDefinition(): InputDefinition
     {
         $definition = parent::getDefaultInputDefinition();
         $definition->addOption(new InputOption('--fcgi', null, InputOption::VALUE_OPTIONAL, 'If specified, used as a connection string to FastCGI server.'));
